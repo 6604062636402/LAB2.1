9e527fe62acf2cb26f9c82b3cab7054e2df52c21
6604062636402 <s6604062636402@email.kmutnb.ac.th>
2025-12-29 16:41:53 +0700

    Add files via upload

---
+++ WS1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d;
+    int max1, max2;
+
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    max1 = (a > b) ? a : b;
+    max1 = (max1 > c) ? max1 : c;
+    max1 = (max1 > d) ? max1 : d;
+
+    max2 = (a != max1) ? a : b;
+    max2 = (max2 > ((b != max1) ? b : c)) ? max2 : ((b != max1) ? b : c);
+    max2 = (max2 > ((c != max1) ? c : d)) ? max2 : ((c != max1) ? c : d);
+    max2 = (max2 > ((d != max1) ? d : max2)) ? max2 : max2;
+
+    printf("%d", max2);
+
+    return 0;
+}
+
